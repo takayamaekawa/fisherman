305822225bcc063fd3a7028262f0a41c861a04e3
bella <maekawatakaya10.8@gmail.com>
2025-05-28 07:57:15 +0900

    del extra

--- app/client.ts
+++ app/client.ts
@@ -1,8 +1,3 @@
 import { createClient } from 'honox/client'
 
-try {
-  createClient();
-} catch (e) {
-  console.error('Error during or after createClient():', e);
-  alert('Error during or after createClient(): ' + (e instanceof Error ? e.message : String(e)));
-}
+createClient()
